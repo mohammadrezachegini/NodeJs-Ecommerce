91de39c57caedb7364c4f1e9d0d52ed7c5160620
Goodarzvand Chegini <goodarzvandcheginim@student.douglascollege.ca>
2023-03-10 10:16:24 -0800

    Create product schema

--- app/models/products.js
+++ app/models/products.js
@@ -2,9 +2,35 @@ const {default: mongoose} = require("mongoose")
 
 const ProductSchema = new mongoose.Schema({
 
+    title : {type: String, required: true},
+    short_dec : {type: String, required: true},
+    full_desc : {type: String, required: true},
+    images : {type: [String], required: true},
+    tags : {type: [String], required: true},
+    category : {type: mongoose.Types.ObjectId, required: true},
+    comments : {type: [], default: []},
+    like : {type: [mongoose.type.ObjectId], default: []},
+    dislike : {type:  [mongoose.type.ObjectId], default: []},
+    bookmark : {type:  [mongoose.type.ObjectId], default: []},
+    price : {type: Number, default: 0},
+    discount : {type: Number, default: 0},
+    count : {type: Number},
+    type : {type: String, required: true},
+    time : {type: String},
+    format : {type: String},
+    instructor : {type:  mongoose.type.ObjectId, required: true},
+    features : {type: Object, default: {
+        length: "",
+        height: "",
+        width: "",
+        weight:"",
+        color: [],
+        models: [],
+        madein: ""
+    }},
 
 });
 
 module.exports = {
-    ProductModel: ProductSchema
+    ProductModel: mongoose.model("product", ProductSchema)
 }
