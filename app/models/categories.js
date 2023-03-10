db7888431a587cda229f442e06b5f115c8780662
Goodarzvand Chegini <goodarzvandcheginim@student.douglascollege.ca>
2023-03-10 10:05:53 -0800

    Create category schema

--- app/models/categories.js
+++ app/models/categories.js
@@ -2,9 +2,10 @@ const {default: mongoose} = require("mongoose")
 
 const CategoriesSchema = new mongoose.Schema({
 
+    title: {type: String, required: true}
 
 });
 
 module.exports = {
-    CategoriesModel: CategoriesSchema
+    CategoriesModel: mongoose.model("category", CategoriesSchema)
 }
