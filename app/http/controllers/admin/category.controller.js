470620fe3cd5315771833e301d26144f67ffb0c9
Reza <mohammadreza.chegini01@gmail.com>
2023-04-27 13:46:24 -0700

    use look up (agreegate) in project

--- app/http/controllers/admin/category.controller.js
+++ app/http/controllers/admin/category.controller.js
@@ -22,6 +22,12 @@ class CategoryController extends Controller {
         }
     }
 
+    async checkExistCategory(id){
+        const category = await CategoryModel.findById(id)
+        if(!category) throw createError.NotFound("Category Not Found")
+        return category
+    }
+
     async removeCategory(req, res,next) {
         try {
             
@@ -38,6 +44,31 @@ class CategoryController extends Controller {
     }
     async getAllCategory(req, res,next) {
         try {
+
+            const category = await CategoryModel.aggregate([
+                {
+                    $lookup: {
+                        from: "categories",
+                        localField: "_id",
+                        foreignField: "parent",
+                        as: "children"
+                    }
+                },
+                {
+                    $project: {
+                        __v: 0,
+                        "children.__v": 0,
+                        "children.parent": 0,
+                    }
+                }
+            ])
+            return res.status(200).json({
+                data:{
+                    statusCode:200,
+                    message:"All Categories",
+                    category
+                }
+            })
             
         } catch (error) {
             next(error);
@@ -52,16 +83,48 @@ class CategoryController extends Controller {
     }
     async getAllCategoriesByParent(req, res,next) {
         try {
-            
+            const parent = await CategoryModel.find({parent: undefined},{__v:0})
+            return res.status(200).json({
+                data:{
+                    statusCode:200,
+                    message:"All Categories",
+                    parent
+                }
+            })
         } catch (error) {
             next(error);
         }
     }
     async getAllChildCategory(req, res,next) {
         try {
-            
+            const {parent} = req.params;
+            const children = await CategoryModel.find({parent},{__v:0, parent:0})
+            return res.status(200).json({
+                data:{
+                    statusCode:200,
+                    message:"All Child Categories",
+                    children
+                }
+            })
         } catch (error) {
             next(error);
+        }
+    }
+
+    async removeCategory(req, res,next) {
+        try {
+            const {id} = req.params;
+            const category = await this.checkExistCategory(id)
+            const deleteResult = await CategoryModel.deleteOne({_id: category._id});
+            if(deleteResult.deletedCount == 0) throw createError.InternalServerError("Unsuccessful Delete category")
+            return res.status(200).json({
+                data:{
+                    statusCode:200,
+                    message:"Category Deleted Successfully"
+                }
+            })
+        } catch (error) {
+            console.log(error);
         }
     }
 
--- app/routers/admin/category.js
+++ app/routers/admin/category.js
@@ -28,6 +28,72 @@ const {CategoryController} = require("../../http/controllers/admin/category.cont
 */
 router.post("/add", CategoryController.addCategory )
 
+/**
+ * @swagger
+ *  /admin/category/parents:
+ *      get:
+ *          summary: get all parent's Category
+ *          tags: [Admin-Panel] 
+ *          description :  get parent's category
+ *          responses:
+ *              201:
+ *                  description : success
+ *             
+*/
+router.get("/parents",CategoryController.getAllCategoriesByParent )
+
+/**
+ * @swagger
+ *  /admin/category/children/{parent}:
+ *      get:
+ *          summary: get all parent's children
+ *          tags: [Admin-Panel] 
+ *          description :  get children's category
+ *          parameters:
+ *              -   in: path
+ *                  required: true
+ *                  name: parent    
+ *                  type: string
+ *          responses:
+ *              201:
+ *                  description : success
+ *             
+*/
+router.get("/children/:parent",CategoryController.getAllChildCategory )
+
+/**
+ * @swagger
+ *  /admin/category/all:
+ *      get:
+ *          summary: get all Categories
+ *          tags: [Admin-Panel] 
+ *          description :  get all categories
+ *          responses:
+ *              201:
+ *                  description : success
+ *             
+*/
+router.get("/all",CategoryController.getAllCategory )
+
+/**
+ * @swagger
+ *  /admin/category/remove/{id}:
+ *      delete:
+ *          summary: delete  Category
+ *          tags: [Admin-Panel] 
+ *          description :  delete Category by id
+ *          parameters:
+ *              -   in: path
+ *                  required: true
+ *                  type: string
+ *                  name: id  
+ *          responses:
+ *              201:
+ *                  description : success
+ *             
+*/
+router.delete("/remove/:id", CategoryController.removeCategory )
+
 module.exports = {
     CategoryRoutes: router
 }
