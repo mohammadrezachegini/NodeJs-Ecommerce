155fcf9f977f82fc6b411a7c1a51e1d536446a62
Reza <mohammadreza.chegini01@gmail.com>
2023-05-09 18:30:21 -0700

    moved swagger to abother directory for api and user

--- app/routers/api/index.js
+++ app/routers/api/index.js
@@ -3,30 +3,6 @@ const {VerifyAccessToken} = require("../../http/middleware/VerifyAccessToken")
 const router = require("express").Router();
 
 
-/**  
- * @swagger
- * tags: 
- *  name: IndexPage
- *  description : index page route and data
- *      
-*/
-/**  
- * @swagger
- * /: 
- *  get:
- *      summary: index of routes
- *      tags: [IndexPage]
- *      description :  get all data that need for index page
- *      parameters:
- *          -   in: header
- *              name: access-token
- *              example: Bearer your token
- *      responses:
- *          200:
- *              description : success
- *          404:
- *              description: Not found
-*/
 
 router.get("/", VerifyAccessToken, HomeController.indexPage)
 
