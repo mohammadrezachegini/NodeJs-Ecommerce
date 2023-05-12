1b88386f1b77d3b6fb375774f7ef48c5b8900daf
Reza <mohammadreza.chegini01@gmail.com>
2023-05-11 21:04:00 -0700

    get All roles

--- app/http/controllers/admin/RBAC/role.controller.js
+++ app/http/controllers/admin/RBAC/role.controller.js
@@ -1,6 +1,24 @@
 const Controller = require('../../controllers')
+const {StatusCodes: HttpStatus} = require("http-status-codes")
+class RoleController extends Controller {
 
-class RoleController extends Controller {}
+    async getAllRoles(req,res,next) {
+        try {
+            // .populate([{path: 'permissions'}])
+            const roles = await RoleModel.find({})
+            return res.status(HttpStatus.OK).json({
+                statusCode: HttpStatus.OK,
+                data: {
+                    message: 'Roles fetched successfully',
+                    roles
+                }
+            })
+            
+        } catch (error) {
+            
+        }
+    }
+}
 
 module.exports = {
     RoleController : new RoleController()
--- app/routers/admin/role.js
+++ app/routers/admin/role.js
@@ -1,6 +1,8 @@
+const { RoleController } = require("../../http/controllers/admin/RBAC/role.controller");
+
 const router = require("express").Router();
 
-
+router.get("/list", RoleController.getAllRoles)
 
 module.exports = {
     RoleAdminApiRoutes : router
